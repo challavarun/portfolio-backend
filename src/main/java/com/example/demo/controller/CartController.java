a46ce65bfdfb8b297e3ce53d6b06678db9d57f5a
challavarun <challavarun305@gmail.com>
2025-09-23 23:09:24 +0530

    cart creation and adding

---
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.CartItemReq;
+import com.example.demo.services.CartService;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@NoArgsConstructor
+@RequestMapping("/api/cart")
+public class CartController {
+    @Autowired
+    CartService cartService;
+    @PostMapping
+    public ResponseEntity<String> creatACart(
+            @RequestHeader("X-user_id") String userId,
+            @RequestBody CartItemReq request){
+        if (!cartService.addToCart(userId,request)){
+            return ResponseEntity.badRequest().body("product out of stock or usernot found");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @DeleteMapping("/item/{id}")
+    public ResponseEntity<Void> dremoveFromCart(
+            @RequestHeader("X-user_id") String userId,
+            @PathVariable Long id
+    ){
+       boolean deleted= cartService.deleteFrromcart(userId,id);
+       return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+
+
+    }
+
+}
---
+++ src/main/java/com/example/demo/services/CartService.java
@@ -0,0 +1,64 @@
+package com.example.demo.services;
+
+import com.example.demo.dto.CartItemReq;
+import com.example.demo.model.CartItems;
+import com.example.demo.model.Product;
+import com.example.demo.model.UserModel;
+import com.example.demo.repo.CartItemRepo;
+import com.example.demo.repo.ProductRepo;
+import com.example.demo.repo.userRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CartService {
+    @Autowired
+    ProductRepo productRepo;
+    @Autowired
+    userRepo userRepo;
+    @Autowired
+    CartItemRepo cartItemRepo;
+
+    public boolean addToCart(String userId, CartItemReq request) {
+        Optional<Product> product = productRepo.findById(request.getProductId());
+        if (product.isEmpty())
+            return false;
+        Product product1 = product.get();
+        if (product1.getStockQuantity() < request.getQuantity())
+            return false;
+        Optional<UserModel> user = userRepo.findById(Long.valueOf(userId));
+        if (user.isEmpty())
+            return false;
+        UserModel user1 = user.get();
+        CartItems existingcartItem = cartItemRepo.findByUserAndProduct(user1,product1);
+        if (existingcartItem != null) {
+            existingcartItem.setQuantity(existingcartItem.getQuantity() + request.getQuantity());
+            existingcartItem.setPrice(existingcartItem.getPrice().multiply(BigDecimal.valueOf(existingcartItem.getQuantity())));
+            cartItemRepo.save(existingcartItem);
+        }
+        else{
+            CartItems cartItems= new CartItems();
+            cartItems.setUser(user1);
+            cartItems.setProduct(product1);
+            cartItems.setQuantity(request.getQuantity());
+            cartItems.setPrice(product1.getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
+            cartItemRepo.save(cartItems);
+        }
+return true;
+    }
+
+    public boolean deleteFrromcart(String userId, Long productId) {
+        Optional<Product> product = productRepo.findById(productId);
+        Optional<UserModel> user = userRepo.findById(Long.valueOf(userId));
+        if (product.isPresent() && user.isPresent()){
+            cartItemRepo.deleteByUserAndProduct(user.get(),product.get());
+        return true;}
+
+        return false;
+    }
+}
